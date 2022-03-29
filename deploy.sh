fd72b0d8246fb72004444e03504db986af4c15ff
tiantingrui <976912095@qq.com>
2022-03-29 09:23:13 +0800

    ci: 修改 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
  
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/tiantingrui/blog main:gh-pages
+git push -f https://github.com/tiantingrui/blog master:gh-pages
  
 cd -
